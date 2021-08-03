ebb9b4c86123f3ce35173b0a208965ba954dad9c
AwalHossain <waqin21@gmail.com>
2021-08-03 12:48:50 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,14 @@
+let roastGiven = 0;
+
+while (roastGiven < 7){
+ 
+console.log("Here, is your roast");
+roastGiven++;
+console.log(roastGiven);
+
+}
+
+var num1= "99.5";
+var num2 = 15;
+
+console.log(num1 + num2);
